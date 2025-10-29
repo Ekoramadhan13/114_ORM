9ca7b6dc7f0b523b4083e9a5ee6db19471c1a808
Ekoramadhan13 <enugraha411@gmail.com>
2025-10-29 12:13:53 +0700

    menambahkan method get

--- index.js
+++ index.js
@@ -34,3 +34,13 @@ app.post('/komik', async (req, res) => {
   }
 });
 
+app.get('/komik', async (req, res) => {
+    try {
+        const komik = await db.Komik.findAll();
+        res.send(komik);
+    } catch (error) {
+        res.send({ message: error.message });
+    }
+});
+
+
